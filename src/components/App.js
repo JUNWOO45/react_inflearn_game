ca6671269144d37c30272c235fa0f8fb20726535
junwoo45 <sinyoorang@gmail.com>
2020-06-04 18:23:38 +0900

    feat: 구구단

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Gugudan from './Gugudan';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>
+        <Gugudan />
+      </div> 
+    );
+  }
+}
+ 
+export default App;
---
+++ src/components/Gugudan.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Gugudan extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      first: Math.ceil(Math.random() * 9),
+      last: Math.ceil(Math.random() * 9),
+      value: '',
+      result: '',
+     }
+  }
+
+  onKeyDown = (e) => {
+    this.setState({ value: Number(e.target.value) });
+  }
+
+  calculate = (e) => {
+    e.preventDefault();
+    return this.state.first * this.state.last === this.state.value ? this.setState({ result: '딩동댕', first: Math.ceil(Math.random() * 9), last: Math.ceil(Math.random() * 9), value: '' }) : this.setState({ result: '땡', value: '' });
+  }
+
+  render() { 
+    return ( 
+      <div>
+        {this.state.first} 곱하기 {this.state.last}는?
+        <form onClick={this.calculate}>
+          <input type="number" value={this.state.value} onChange={this.onKeyDown}/>
+          <button>입력</button>
+        </form>
+        <div>
+          결과: {this.state.result}
+        </div>
+      </div> 
+    );
+  }
+}
+ 
+export default Gugudan;
