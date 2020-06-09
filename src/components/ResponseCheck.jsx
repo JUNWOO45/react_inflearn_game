81f03d81bb3e2f7f8fe3978cab951793a417b964
junwoo45 <sinyoorang@gmail.com>
2020-06-09 16:54:37 +0900

    feat: 반응속도측정

--- src/components/ResponseCheck.jsx
+++ src/components/ResponseCheck.jsx
@@ -8,8 +8,47 @@ class ResponseCheck extends Component {
     result: []
   };
 
-  onClick = () => {
+  timeout;
+  startTime;
+  endTime;
 
+  onClick = () => {
+    const { state, message, result } = this.state;
+    if(state === 'waiting') {
+      this.setState({ 
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.'
+      });
+
+      this.timeout = setTimeout(() => {
+        this.setState({ 
+          state: 'now',
+          message: '지금 클릭!'
+        });
+        
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+
+    } else if(state === 'ready') {
+      this.setState({ 
+        state: 'waiting',
+        message: '지금 누르지 마세요.. 다시 시작하려면 클릭!'
+      });
+
+      clearTimeout(this.timeout);
+
+    } else if(state === 'now') {
+      this.endTime = new Date();
+      console.log(this.endTime - this.startTime);
+      this.setState( prevState => {
+        
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작해보세요.',
+          result: [...prevState.result, this.endTime - this.startTime]
+        }
+      });
+    }
   };
 
   renderAverage = () => {
